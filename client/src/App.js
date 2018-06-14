a6d6458fa3a5eba3a2579da6d762e025f9af1add
Kyle Helle <kjhelle4@gmail.com>
2018-06-13 18:49:37 -0700

    make text result bigger

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <p className="Speech">
           {this.state.response}
         </p>
       </div>
